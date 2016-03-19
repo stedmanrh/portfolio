5a420b9bc75a3a5d264cbe11ed6780673aeb3197
Stedman <stedman.rh@gmail.com>
2016-03-18 22:31:12 -0700

    nav li spacing fix

--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,14 @@
 $(document).ready(function(){
 
     var height = $('header svg').offset().top;
-    // var logotypeHeight = $('.logotype').height();
 
     $(window).scroll(function(){
         var scroll = $(this).scrollTop();
         if (scroll >= height){
             $('header').addClass('sticky');
-            // $('.logotype').hide();
         }
         else {
             $('header').removeClass('sticky');
-            // $('.logotype').show();
         }
     });
 });
