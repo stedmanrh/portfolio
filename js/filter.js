d991232e7dd539f0656a74beaeb8620b3ce440c3
Stedman <stedman.rh@gmail.com>
2016-03-26 01:36:20 -0700

    filter works

--- js/filter.js
+++ js/filter.js
@@ -1,3 +1,30 @@
+function filter(){
+    // $('.plink .link').fadeOut('slow');
+    var currentTags = [];
+    $('.filter-link.current').each(function(){
+        var tag = $(this).data("tag");
+        currentTags.push(tag);
+    });
+
+    $('.plink').each(function(){
+        var plink = $(this);
+        var show = false;
+        for(var i=0; i<currentTags.length; i++){
+            if (plink.find('.link').hasClass(currentTags[i])){
+                show = true;
+                break;
+            }
+        }
+        if(show){
+            plink.find('.link').fadeIn();
+        }
+        else {
+            plink.find('.link').fadeOut();
+        }
+    });
+
+}
+
 $(document).ready(function(){
 
     $('.filter-link').click(function(){
@@ -5,7 +32,7 @@ $(document).ready(function(){
             if(!($(this).hasClass("current"))){
                 $('.filter-link').removeClass("current");
                 $(this).addClass("current");
-                // load projects
+                $('.plink .link').fadeIn();
             }
         }
         else {
@@ -13,6 +40,10 @@ $(document).ready(function(){
             $(this).toggleClass("current");
             if(!($('.filter-link').hasClass('current'))){
                 $('.filter-link.all').addClass('current');
+                $('.plink .link').fadeIn();
+            }
+            else {
+                filter();
             }
         }
     });
