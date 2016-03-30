cdfe01a638aad28857ed4a5b5ffd49df50e1d578
Stedman <stedman.rh@gmail.com>
2016-03-30 08:34:48 -0700

    masonry init after each image load (consistent layouts on page reloads with smoothState)

--- js/transitions.js
+++ js/transitions.js
@@ -1,11 +1,16 @@
 function cascade(){
-    $('.grid').masonry({
+    var $grid = $('.grid').masonry({
       // set itemSelector so .grid-sizer is not used in layout
       itemSelector: '.grid-item',
       // use element for option
       columnWidth: '.grid-sizer',
       percentPosition: true,
       gutter: '.gutter-sizer'
+    });
+
+    // layout Masonry after each image loads
+    $grid.imagesLoaded().progress( function() {
+      $grid.masonry('layout');
     });
 }
 
