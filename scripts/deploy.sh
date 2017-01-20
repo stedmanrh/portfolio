480b18929bd8f67492aa46cb58bbf1df74074bf5
Stedman Halliday <stedman.rh@gmail.com>
2017-01-19 23:59:01 -0800

    deploy script fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 jekyll build
 cd _site
-rsync -avru --rsh='ssh -p2222' . stedman@stedmanhalliday.com:public_html --delete-before
+rsync -avru --rsh='ssh -p2222' . stedman@50.87.147.174:public_html --delete-before
 cd ..
